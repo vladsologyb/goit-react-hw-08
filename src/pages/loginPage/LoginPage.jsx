cde61ff8e43fc6a9667502201b870ac9eba91fbe
Влад Сологуб <143345251+vladsologyb@users.noreply.github.com>
2024-07-13 15:45:19 +0300

    corrected

--- src/pages/loginPage/LoginPage.jsx
+++ src/pages/loginPage/LoginPage.jsx
@@ -1,6 +1,7 @@
-import LoginForm from '../../components/LoginForm/LoginForm';
+
 import PageTitle from "../../components/pageTitle/PageTitle";
 import css from "./LoginPage.module.css";
+import LoginForm from '../../components/LoginForm/LoginForm';
 
 export default function LoginPage() {
   return (
